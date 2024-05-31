4441f0511e0a73ee040266d639a306132ed34a95
jeehay <sophiepark528@gmail.com>
2024-06-01 05:36:41 +0900

    feat: add query

---
+++ src/api/factCheck.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "react-query";
+
+// export const useFactCheck = () => {
+//   const url = 'https://jsonplaceholder.typicode.com/todos/1';
+//   return useQuery("factCheck", () =>
+//     fetch(url).then((res) => {
+//       if (!res.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return res.json();
+//     })
+//   );
+// };
+
+
+export const useFactCheck = (userInput: string) => {
+    const url = 'http://172.190.90.75:5000/get-result';
+    const data = { url: userInput };
+   
+    
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+
+    // Invoke useQuery inside a React component function
+    return useQuery("factCheck", async () => {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    });
+};
